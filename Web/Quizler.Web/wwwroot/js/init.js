6ac8f9d5baef21a31973af9b6be2a40a681da453
Kristian Ganchev <kris.ganchev91@gmail.com>
2020-04-10 19:14:39 +0300

    Implement create quiz. UI improvements.

---
+++ Web/Quizler.Web/wwwroot/js/init.js
@@ -0,0 +1,17 @@
+(function ($) {
+    $(function () {
+
+        $('.sidenav').sidenav();
+
+    }); // end of document ready
+
+    $(document).ready(function () {
+        $('select').formSelect();
+    });
+
+    $(document).ready(function () {
+        $('.modal').modal();
+    });
+
+})(jQuery); // end of jQuery name space
+
